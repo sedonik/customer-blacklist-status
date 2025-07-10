a2f0f401741c56a6610121c6a5ba6e4dee055d5a
sdovbenko <sergey.dovbenko@gmail.com>
2025-07-10 13:33:19 +0300

    Add Configs

---
+++ app/code/Greentube/Brevo/Model/Config/Source/SyncFrequency.php
@@ -0,0 +1,22 @@
+<?php
+namespace Greentube\Brevo\Model\Config\Source;
+
+use Magento\Framework\Data\OptionSourceInterface;
+
+class SyncFrequency implements OptionSourceInterface
+{
+    /**
+     * Get options
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return [
+            ['value' => '0', 'label' => __('Not sync')],
+            ['value' => '1', 'label' => __('Every hour')],
+            ['value' => '12', 'label' => __('Twice a day')],
+            ['value' => '24', 'label' => __('Once a day')]
+        ];
+    }
+} 
