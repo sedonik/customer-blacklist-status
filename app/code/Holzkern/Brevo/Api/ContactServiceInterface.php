6c17f0befff1f0247e19fb626056155844b63a3f
sdovbenko <sergey.dovbenko@gmail.com>
2025-07-12 18:46:36 +0300

    Implemented methods: getAll, syncById, syncAll in Service

--- app/code/Holzkern/Brevo/Api/ContactServiceInterface.php
+++ app/code/Holzkern/Brevo/Api/ContactServiceInterface.php
@@ -12,4 +12,29 @@ interface ContactServiceInterface
      * @return array
      */
     public function getById($brevoId);
+
+    /**
+     * Get all contacts with pagination
+     *
+     * @param int $limit
+     * @param int $offset
+     * @param string $sort
+     * @return array
+     */
+    public function getAll($limit = 50, $offset = 0, $sort = 'asc');
+
+    /**
+     * Sync contact blacklist status by Brevo ID
+     *
+     * @param int $brevoId
+     * @return void
+     */
+    public function syncById($brevoId);
+
+    /**
+     * Sync all contacts in batches
+     *
+     * @return array
+     */
+    public function syncAll();
 }
